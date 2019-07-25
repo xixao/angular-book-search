0f01afd9e05a1e92e43755a982701e369a1d6cb1
Steve Melcher <smelcher@belletire.com>
2019-07-25 13:13:33 -0400

    Header and base styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BooksListComponent } from './books-list/books-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BooksListComponent
   ],
   imports: [
     BrowserModule
